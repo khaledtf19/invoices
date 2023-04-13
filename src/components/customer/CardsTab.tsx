088820f2f3b9ed952341585d31adb4f8b5f314d8
Khaledtf Labeb <khaledtf19@gmail.com>
2023-04-14 01:13:42 +0200

    update cutomer tabs

--- src/components/customer/CardsTab.tsx
+++ src/components/customer/CardsTab.tsx
@@ -1,16 +1,20 @@
 import Container from "../../container/Container";
 import { BsFillChatSquareTextFill } from 'react-icons/bs'
 import { RouterOutputs } from "../../utils/trpc";
+import { useState } from "react";
+import { IconToCopy } from "../utils";
 
 const CardsTab: React.FC<{ customerData: RouterOutputs["customer"]["getCustomerById"] }> = ({ customerData }) => {
   const date = new Date()
+
   if (!customerData) {
     return <></>
   }
+
   return (
-    <div className="flex justify-end gap-3 w-full">
+    <div className="flex flex-row-reverse justify-start gap-3 w-full">
       <TextToCopy text={`${date.getHours() >= 12 ? "مساء" : "صباح"} الخير لو سمحت عايز اشحن كروت للرقم ده ${customerData.number}`} label="Text1" />
-      <TextToCopy text={``} label="Text2" />
+      <TextToCopy text={""} label="Text2" />
     </div>
   )
 }
@@ -18,12 +22,13 @@ const CardsTab: React.FC<{ customerData: RouterOutputs["customer"]["getCustomerB
 export default CardsTab
 
 const TextToCopy: React.FC<{ text: string, label: string }> = ({ text, label }) => {
+  const [textS, setTextS] = useState(text)
   return <div className="text-lg flex items-center justify-center flex-col">
     <Container>
       <label>
-        {label}
+        <IconToCopy name="Text" text={textS} Icon={BsFillChatSquareTextFill} size={20} />
       </label>
-      <p className="bg-black  p-2 text-right  text-white w-full resize-y min-h-[100px]" contentEditable={true}>{text}</p>
+      <textarea className="bg-black  p-2 text-right  text-white w-full resize-y  min-h-[100px]" onChange={(e) => { setTextS(e.target.value) }} value={textS}></textarea>
     </Container>
   </div>
 }
