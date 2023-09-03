6d0b95073546840c0572f007db2bb4bbb3a71eb5
Khaledtf Labeb <khaledtf19@gmail.com>
2023-09-03 21:17:43 +0300

    update prisma, perttier

--- src/pages/bank/work.tsx
+++ src/pages/bank/work.tsx
@@ -1,5 +1,42 @@
-const Work = ()=>{
-  return <div>work</div>
-}
+import * as React from "react";
 
-export default Work
+import { RedButton } from "../../components/utils";
+import Container from "../../container/Container";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Work = () => {
+  const [cells, setcells] = React.useState([0]);
+
+  return (
+    <Container>
+      <div className="w-full flex">
+        <CellContainer />
+      </div>
+    </Container>
+  );
+};
+
+export default Work;
+export const CellContainer = () => {
+  return (
+    <div className="flex w-full gap-2">
+      <Cell />
+      <div className="w-10">
+        <RedButton label="X" />
+      </div>
+    </div>
+  );
+};
+
+export const Cell = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ type, className, ...props }) => {
+    return (
+      <input
+        type={type}
+        className="w-full border border-gray-900 p-1 outline-none"
+      />
+    );
+  }
+);
